0df807d5a47406a25e1eaaf4ddff0591eee17db1
gliese-backup <glieseb+1@proton.me>
2024-12-09 14:22:06 +0000

    Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 function Header() {
   return (
-    <header className="flex gap-2">
-      <i>ICON</i>
-      <p>Untitled UI</p>
+    <header className="flex items-center gap-2">
+      <img src="favicon.png" className="w-10" />
+      <p className="text-lg font-semibold">Untitled UI</p>
     </header>
   );
 }

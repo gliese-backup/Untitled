2fb8372d72f157b2da0502a06991e0f263b57055
gliese-backup <glieseb+1@proton.me>
2024-12-09 14:04:20 +0000

    Layout done

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,13 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
+import Home from "./pages/Home";
 
 const appEl = document.querySelector("#app");
 const root = createRoot(appEl);
 
 root.render(
   <React.StrictMode>
-    <h1 className="cursor-pointer text-2xl font-bold text-zinc-900 hover:text-red-800 md:text-4xl">
-      I am a duster
-    </h1>
+    <Home />
   </React.StrictMode>,
 );
---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import Header from "../components/Header";
+import Aside from "../components/Aside";
+import Form from "../components/Form";
+
+function Home() {
+  return (
+    <div>
+      <Header />
+      <Aside />
+      <Form />
+    </div>
+  );
+}
+
+export default Home;
